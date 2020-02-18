e64a0f080ed8ab2f7261d0d9c5ce3a0edd017e81
Beppe Vanrolleghem <beppe.vanrolleghem@gmail.com>
2020-02-18 09:57:39 +0100

    build test

---
+++ serverA/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+URL = "http://service"
+
+
+@app.route('/')
+def doRequest():
+    return requests.get(URL).json()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
