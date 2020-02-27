896ae57a881dd7961bfc7d9a4e73c6e83cd83b30
Beppe Vanrolleghem <beppe.vanrolleghem@gmail.com>
2020-02-27 19:17:44 +0100

    changes

--- serverA/app.py
+++ serverA/app.py
@@ -14,6 +14,9 @@ def doRequest():
 def itWorks():
     return requests.get(URL).json()
 
+@app.route('/find/<name>/<port>')
+def findServer(name, port):
+    return requests.get("http://"+name+":"+port)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
