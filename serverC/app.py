ed479f3f0475f43cab7811667ebb3784ad157857
Beppe Vanrolleghem <beppe.vanrolleghem@gmail.com>
2020-02-27 11:07:34 +0100

    terug naar start

---
+++ serverC/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def doRequest():
+    data = {
+        "serverName": "serverC",
+        "success": "true"
+    }
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=6000)
