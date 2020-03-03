bbe673d32ba5b9e650e2ff5d2fc72306ab4ee49a
Beppe Vanrolleghem <beppe.vanrolleghem@gmail.com>
2020-03-03 20:33:28 +0100

    trigger update

--- serverA/app.py
+++ serverA/app.py
@@ -5,10 +5,21 @@ app = Flask(__name__)
 
 URL = "http://server-check:6000"
 
+@app.errorhandler(404)
+def page_not_found(error):
+   return "beppe's 404 error"
 
 @app.route('/')
 def doRequest():
     return "it works"
+
+@app.route('/server-a')
+def checkThis():
+	return "server-a is apparently something to worry abt"
+
+@app.route("/server-a/check")
+def itAlsoWorks():
+	return requests.get(URL).json()
 
 @app.route('/check')
 def itWorks():
